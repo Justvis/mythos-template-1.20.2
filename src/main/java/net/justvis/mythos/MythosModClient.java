c1cf465e873fa61817cc222009db6feb684b1668
Justvis <noahjzuko@gmail.com>
2023-10-19 21:45:40 -0400

    Initial commit

---
+++ src/main/java/net/justvis/mythos/MythosModClient.java
@@ -0,0 +1,10 @@
+package net.justvis.mythos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MythosModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
